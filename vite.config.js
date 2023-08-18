7a75092f0711f8f68ad3a290ea646f0509644446
Lukas Stracke <lukas.stracke@sentry.io>
2023-08-18 23:24:46 +0200

    add sentry and route

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
+import { sentrySvelteKit } from '@sentry/sveltekit';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sentrySvelteKit({
+			sourceMapsUploadOptions: {
+				org: 'personal-frn',
+				project: 'javascript-react'
+			},
+			autoInstrument: true
+		}),
+		sveltekit()
+	],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
